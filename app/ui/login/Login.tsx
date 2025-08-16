399a70f998db3233604a1963b69691bfaad9fc29
Priyanshu Chourasia <priyanshuchourasia916@gmail.com>
2025-08-16 12:41:13 +0530

    My mistakes of leaving variables empty

--- app/ui/login/Login.tsx
+++ app/ui/login/Login.tsx
@@ -14,7 +14,7 @@ export default function Login(){
         <div className="py-8 px-6 grid grid-cols-2">
             <div className="py-6 px-4">
                 <div className="flex justify-center flex-col">
-                    <h1 className="text-4xl mb-4 font-semibold text-center dark:text-white text-black">Welcome Back</h1>
+                    <h1 className="text-4xl mb-4 font-semibold text-center dark:text-white text-black">Welcome Back Its a push via CI/CD Pipeline</h1>
                     <span className="text-center">
                         Simplify your workflow and boost your productivity with WishAlpha App. Get started for free.
                     </span>
